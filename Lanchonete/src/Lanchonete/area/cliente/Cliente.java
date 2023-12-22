bfb6a59dcbd3db262785f998fe61be14b55ed239
jaumg2004 <151750450+jaumg2004@users.noreply.github.com>
2023-12-22 11:17:12 -0300

    Add files via upload

---
+++ Lanchonete/src/Lanchonete/area/cliente/Cliente.java
@@ -0,0 +1,19 @@
+package Lanchonete.area.cliente;
+
+public class Cliente {
+    void escolherLanche() {
+        System.out.println("ESCOLHENDO O LANCHE");
+    }
+    void fazerPedido() {
+        System.out.println("FAZENDO O PEDIDO");
+    }
+    void pagarConta() {
+        System.out.println("PAGANDO A CONTA");
+    }
+    public void consultarSaldoAplicativo() {
+        System.out.println("CONSULTANDO SALDO NO APLICATIVO");
+    }
+    public void pegarPedidoBalcao() {
+        System.out.println("PEGANDO O PEDIDO NO BALCÃO");
+    }
+}
